34475377a91f2f9788ae71cde0b85ea1ecb8779a
Hayato Nakamura <hanakamu@c1r6s6.42tokyo.jp>
2025-12-09 21:20:42 +0900

    add

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 20:31:52 by hanakamu          #+#    #+#             */
+/*   Updated: 2025/12/09 20:55:37 by hanakamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*prompt;
+
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_putstr_fd("Usage: ./minishell", 2);
+		return (EXIT_FAILURE);
+	}
+	while (1)
+	{
+		prompt = readline("minishell> ");
+		if (prompt == NULL)
+			exit(EXIT_FAILURE);
+		execute_prompt(prompt); /* parse prompt and execute command */
+		add_history(line);
+		free(line);
+	}
+	return (EXIT_SUCCESS);
+}
