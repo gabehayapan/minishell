f03f60a3bd52239c1f48abfca3816c74f7532901
hayato <nkmr.hayabusa.23@gmail.com>
2025-12-20 11:19:07 +0900

    new: add files in tokenize/

---
+++ tokenize/tokenize.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 18:01:56 by hanakamu          #+#    #+#             */
+/*   Updated: 2025/12/19 16:40:01 by hanakamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKENIZE_H
+# define TOKENIZE_H
+
+# define SUCCESS 0
+# define FAILURE 1
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include "../libft/header/libftall.h"
+
+typedef enum e_tk_type
+{
+	SINGLE_REDIRECTION,	/* >, < */
+	DOUBLE_REDIRECTION,	/* <<, >> */
+	SINGLE_HISTORY,		/* ! */
+	DOUBLE_HISTORY,		/* !! */
+	WILDCARD,			/* * */
+	PIPE,				/* | */
+	SINGLE_QUOTE,		/* ' */
+	DOUBLE_QUOTE,		/* " */
+	DOLLAR,				/* $ */
+	OPTION,				/* - */
+	PARENTHESIS,		/* () */
+	BRACES,				/* {} */
+	LOGICAL_OPERATOR,	/* &&, || */
+	TILDE,				/* ~ */
+	END,				/* \0 */
+	WORD,				/* string */
+	SPACES,				/* white space */
+}	t_tk_type;
+
+typedef struct s_token
+{
+	t_tk_type		tk_type;
+	char			*word;
+	struct s_token	*next;
+}	t_token;
+
+int	ft_isspace(int c);
+
+#endif
