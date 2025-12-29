4c9a240308c6a2121765819fb749c8088eefa68d
hayato <nkmr.hayabusa.23@gmail.com>
2025-12-29 10:14:10 +0900

    add: tokenizer

---
+++ tokenizer/.main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   .main.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/28 16:40:06 by hanakamu          #+#    #+#             */
+/*   Updated: 2025/12/28 16:43:21 by hanakamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+int	main(void)
+{
+	t_token	*tokens;
+	t_token	*tmp;
+	char	*line;
+
+	line = readline("prompt> ");
+	printf("%s\n", line);
+	tokens = tokenizer(line);
+	if (tokens == NULL)
+	{
+		printf("error at tokenizer\n");
+		return (1);
+	}
+	tmp = tokens;
+	while (tmp != NULL)
+	{
+		printf("word:%s, token type:%d\n", tmp->word, tmp->tk_type);
+		tmp = tmp->next;
+	}
+	free(line);
+	free(tokens);
+	return (0);
+}
---
+++ tokenizer/tokenizer.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 11:54:45 by hanakamu          #+#    #+#             */
+/*   Updated: 2025/12/21 14:06:04 by hanakamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenizer.h"
+
+int	get_word(char **str, t_token *new_token, t_tk_type tk_type)
+{
+	int		len;
+	char	*tmp;
+
+	tmp = *str;
+	if (tk_type == DOUBLE_REDIRECTION || tk_type == DOUBLE_HISTORY
+		|| tk_type == LOGICAL_OPERATOR)
+		len = 2;
+	else if (tk_type == WORD)
+	{
+		len = 0;
+		while (get_token_type(tmp) == WORD)
+			tmp++;
+		len = tmp - *str;
+	}
+	else
+		len = 1;
+	new_token->word = ft_substr(*str, 0, len);
+	if (new_token->word == NULL)
+		return (FAILURE);
+	*str = *str + len;
+	return (SUCCESS);
+}
+
+t_token	*create_new_token(char **str, t_token *current, t_tk_type tk_type)
+{
+	t_token	*new_token;
+
+	new_token = (t_token *)malloc(sizeof(t_token));
+	if (new_token == NULL)
+		return (NULL);
+	if (get_word(str, new_token, tk_type) == FAILURE)
+	{
+		free(new_token);
+		return (NULL);
+	}
+	new_token->tk_type = tk_type;
+	new_token->next = NULL;
+	current->next = new_token;
+	return (new_token);
+}
+
+t_token	*tokenizer(char *str)
+{
+	t_token		head;
+	t_token		*current;
+	t_tk_type	tk_type;
+
+	if (str == NULL)
+		return (NULL);
+	head.next = NULL;
+	current = &head;
+	while (*str != '\0')
+	{
+		while (ft_isspace(*str))
+			str++;
+		tk_type = get_token_type(str);
+		current = create_new_token(&str, current, tk_type);
+		if (current == NULL)
+			return (free_token(head.next));
+		if (tk_type == SINGLE_QUOTE || tk_type == DOUBLE_QUOTE)
+		{
+			current = handle_quote(&str, current, tk_type);
+			if (current == NULL)
+				return (free_token(head.next));
+		}
+	}
+	return (head.next);
+}
