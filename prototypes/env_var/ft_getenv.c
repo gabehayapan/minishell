8644c4d73956b030a93de0fcdf987083eaccd46d
nkhyt <nkmr.hayato.23@gmail.com>
2025-12-30 22:15:15 +0900

    new: created ft_getenv

--- prototypes/env_var/ft_getenv.c
+++ prototypes/env_var/ft_getenv.c
@@ -11,3 +11,24 @@
 /* ************************************************************************** */
 
 #include "env_var.h"
+
+int	*ft_strcmp(const char *s1, const char *s2)
+{
+	while ((unsigned char)*s1 == (unsigned char)*s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+char	*ft_getenv(t_env *env_lst, char *target)
+{
+	while (env_lst != NULL)
+	{
+		if (strcmp(env_lst->key, target) == 0)
+			return (env_lst->value);
+		env_lst = env_lst->next;
+	}
+	return (NULL);
+}
