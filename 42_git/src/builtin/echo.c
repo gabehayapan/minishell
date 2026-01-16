f8c042b68b42d7cfdb599917958f564524270a50
hayato <nkmr.hayabusa.23@gmail.com>
2026-01-16 11:02:44 +0900

    new: echo

--- 42_git/src/builtin/echo.c
+++ 42_git/src/builtin/echo.c
@@ -6,13 +6,69 @@
 /*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 20:21:48 by hanakamu          #+#    #+#             */
-/*   Updated: 2026/01/15 20:23:49 by hanakamu         ###   ########.fr       */
+/*   Updated: 2026/01/16 11:02:12 by hanakamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+#include "ft_dprintf.h"
+#include <stdbool.h>
 
-int	echo(char **strs)
+int	is_valid_option(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str != 'n')
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
+int	check_option(char ***strs)
+{
+	int		is_option;
+	int		is_valid;
+	char	**argv;
+
+	is_option = 0;
+	argv = *(strs + 1);
+	while (**argv == '-')
+	{
+		is_valid = is_valid_option((*argv) + 1);
+		if (is_valid == true)
+			is_option = 1;
+		else
+		{
+			*strs = argv;
+			break ;
+		}
+		argv++;
+	}
+	return (is_option);
+}
+
+int	echo(char **strs, int fd)
 {
 	int	is_option;
+	int	is_space;
+
+	is_option = check_option(&strs);
+	is_space = false;
+	while (*strs != NULL && *(strs + 1) != NULL)
+	{
+		ft_dprintf(fd, "%s", *strs);
+		if (is_space == false)
+			is_space = true;
+		strs++;
+	}
+	if (*strs != NULL)
+	{
+		if (is_space == true)
+			ft_dprintf(fd, " ");
+		ft_dprintf(fd, "%s", *strs);
+	}
+	if (is_option == false)
+		ft_dprintf(fd, "\n");
+	return (SUCCESS);
 }
