df227184202bc23747a525e3a2c96dbd0dd35d67
hayato <nkmr.hayabusa.23@gmail.com>
2026-01-21 09:43:55 +0900

    fix: parser.h

--- 42_git/include/parser.h
+++ 42_git/include/parser.h
@@ -6,7 +6,7 @@
 /*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 09:27:16 by hanakamu          #+#    #+#             */
-/*   Updated: 2026/01/21 06:15:07 by hanakamu         ###   ########.fr       */
+/*   Updated: 2026/01/21 09:43:31 by hanakamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,12 +118,11 @@ int			expand_quoted_dollar(t_token **tokens, t_token *current,
 int			handle_dbl_quoted_dollar(t_token **tokens, t_token *current,
 				t_env *env_lst, long exit_status);
 
-// handle_input/parser/expand_wildcard.c
-int			get_dir_ent(t_token *current, char *cwd);
-
 // handle_input/parser/init_wildcard.c
-char		*get_target_dir(void);
+char		*get_target_dir(t_token **current);
 
+// handle_input/parser/wildcard_retrieve.c
+int			get_matching_files(t_token **tokens, t_token *head, char *cwd);
 
 // handle_input/parser/handle_exit_status.c
 char		*handle_exit_status(t_token **next, long exit_status);
