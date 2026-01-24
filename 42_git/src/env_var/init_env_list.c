62a8df39c798bc6682476f52627c7a9cdd11a7be
hayato <nkmr.hayabusa.23@gmail.com>
2026-01-24 15:39:50 +0900

    fix: norm error in init_env_list.c

--- 42_git/src/env_var/init_env_list.c
+++ 42_git/src/env_var/init_env_list.c
@@ -6,11 +6,25 @@
 /*   By: hanakamu <hanakamu@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 13:57:47 by hanakamu          #+#    #+#             */
-/*   Updated: 2026/01/23 19:23:16 by hanakamu         ###   ########.fr       */
+/*   Updated: 2026/01/24 15:39:25 by hanakamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env_var.h"
+
+int	set_env_member(t_env *current, char *envp)
+{
+	int	is_success;
+
+	is_success = new_env_var(current, envp);
+	if (is_success == FAILURE)
+		return (FAILURE);
+	if (ft_strcmp(envp, "_") != 0)
+		current->is_env = true;
+	else
+		current->is_env = false;
+	return (SUCCESS);
+}
 
 t_env	*init_env_list(char **envp)
 {
@@ -26,14 +40,12 @@ t_env	*init_env_list(char **envp)
 			return (NULL);
 		if ((init_vars.head).next == NULL)
 			(init_vars.head).next = init_vars.current;
-		is_success = new_env_var(init_vars.current, *envp);
+		is_success = set_env_member(init_vars.current, *envp);
 		if (is_success == FAILURE)
 		{
 			free_env_lst((init_vars.head).next);
 			return (NULL);
 		}
-		if (ft_strcmp(*envp, "_") != 0)
-			(init_vars.current)->is_env = true;
 		if (init_vars.last != NULL)
 			(init_vars.last)->next = init_vars.current;
 		init_vars.last = init_vars.current;
