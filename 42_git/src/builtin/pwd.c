2bafe0be75cf56079e806953d892459e22b9c9e7
Ison Kislovodsk <ison.c.2012.tk@gmail.com>
2026-01-31 20:23:17 +0900

    add ISLNK

--- 42_git/src/builtin/pwd.c
+++ 42_git/src/builtin/pwd.c
@@ -6,12 +6,32 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 19:32:04 by keitotak          #+#    #+#             */
-/*   Updated: 2026/01/31 09:54:40 by hanakamu         ###   ########.fr       */
+/*   Updated: 2026/01/31 20:17:41 by keitotak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env_var.h"
 #include "ft_dprintf.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+int	lstat(const char *pathname, struct stat *statbuf);
+
+int	is_symbolic_link(const char *pathname)
+{
+	struct stat	sb;
+
+	if (lstat(pathname, &sb) < 0)
+	{
+		perror("lstat");
+		return (0);
+	}
+	if (S_ISLNK(sb.st_mode))
+		return (1);
+	return (0);
+}
+
 
 int	pwd(t_env **env_lst)
 {
@@ -20,12 +40,18 @@ int	pwd(t_env **env_lst)
 
 	pwd = env_find(*env_lst, "PWD");
 	if (pwd != NULL)
+	{
+		if (is_symbolic_link(pwd->value))
+			ft_dprintf(STDOUT_FILENO, "symbolic link.\n");
 		ft_dprintf(STDOUT_FILENO, "%s\n", pwd->value);
+	}
 	else
 	{
 		cwd = getcwd(NULL, 0);
 		if (cwd == NULL)
 			return (EXIT_FAILURE);
+		if (is_symbolic_link(cwd))
+			ft_dprintf(STDOUT_FILENO, "symbolic link.\n");
 		ft_dprintf(STDOUT_FILENO, "%s\n", cwd);
 		free(cwd);
 	}
