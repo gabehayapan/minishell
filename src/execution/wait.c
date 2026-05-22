f2bb241a2c8e3fcb5bbc1abe92d3e7cb6a153bf9
hayato <nkmr.hayabusa.23@gmail.com>
2026-05-22 19:05:29 +0900

    fix: initialize variable in wait.c

--- src/execution/wait.c
+++ src/execution/wait.c
@@ -6,7 +6,7 @@
 /*   By: keitotak <keitotak@student.42tokyo.jp      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 20:34:18 by keitotak          #+#    #+#             */
-/*   Updated: 2026/01/27 11:53:48 by hanakamu         ###   ########.fr       */
+/*   Updated: 2026/05/22 19:04:43 by hanakamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int	wait_for_children(int *procid, int proc_count)
 
 	(void)procid;
 	i = 0;
+	wstatus = 0;
 	while (i < proc_count)
 	{
 		if (wait(&wstatus) == error)
